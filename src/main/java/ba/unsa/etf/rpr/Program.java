850aeec1f4cd703a9fc8e346df52c4559a0968db
EmirKapic <ekapic1@etf.unsa.ba>
2022-10-31 17:44:11 +0100

    Initial commit

---
+++ src/main/java/ba/unsa/etf/rpr/Program.java
@@ -0,0 +1,8 @@
+package ba.unsa.etf.rpr;
+
+public class Imenik {
+    public static void main(String[] args) {
+        FiksniBroj f = new FiksniBroj(Grad.Bihac,"537-487");
+        System.out.println(f.hashCode());
+    }
+}
